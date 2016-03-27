1821a697ec8ac21a25ddf1f1727740e516f9da05
Shriraj <shri.93.raj@gmail.com>
2016-03-27 18:36:27 +0000

    Article form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 root 'pages#index'
   get 'about' => 'pages#about'
+  
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
