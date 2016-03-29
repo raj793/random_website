75154675e32b6d0dc1b125e5de63bcb495f2ea01
Shriraj <shri.93.raj@gmail.com>
2016-03-29 22:18:25 +0000

    Association and debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,9 @@ before_action :set_article, only: [:edit, :update, :show, :destroy]
   end
 
   def create
+    debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created."
       redirect_to article_path(@article)
