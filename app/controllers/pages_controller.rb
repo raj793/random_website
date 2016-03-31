56b8b5e3c879b1df4810e4eb4196f4ea4559e8d8
Shriraj <shri.93.raj@gmail.com>
2016-03-31 00:36:14 +0000

    complete authentication system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def index
+    
+    redirect_to articles_path if logged_in?
   end
   
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ root 'pages#index'
  
  get 'signup', to: 'users#new'
  resources :users, except: [:new]
+ 
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
